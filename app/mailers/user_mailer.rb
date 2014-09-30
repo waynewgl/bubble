b48db1400415f246eeb674acbaf311c78c3125d0
way <waynewgl@gmail.com>
2014-09-30 21:26:42 +0800

    update

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 #encoding: UTF-8
 
 class UserMailer < ActionMailer::Base
-  default from: "waynewgl@163.com"
+  default from: "appscreator@163.com"
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
@@ -11,6 +11,5 @@ class UserMailer < ActionMailer::Base
 
     @user = user
     mail(:subject => "#{subject}", :to => user[:account])
-
   end
 end
