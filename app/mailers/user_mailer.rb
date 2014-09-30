79b4e69245c37bb7c117092a3e36611d49cb9be2
way <waynewgl@gmail.com>
2014-09-30 19:49:13 +0800

    update

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,5 @@
 class UserMailer < ActionMailer::Base
   default from: "app__developer@163.com"
-
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
