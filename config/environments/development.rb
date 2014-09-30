93b789b8fcea2357adaecb3c05bb8ddd7bdde9a7
way <waynewgl@gmail.com>
2014-09-30 19:52:04 +0800

    update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ Meets::Application.configure do
       :port => "25",
       :domain => "163.com",
       :authentication => :login,
-      :user_name => "app__developer",
+      :user_name => "waynewgl",
       :password => "zero06120707",
       :enable_starttls_auto => true
   }
