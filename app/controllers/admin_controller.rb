8be1ade9a1a565267cf351a4f65763e2f1c173a5
way <waynewgl@gmail.com>
2014-11-07 12:08:06 +0800

    update

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -59,7 +59,7 @@ class AdminController < ApplicationController
       user_dic[:id] = user.id
       user_dic[:nickname]  = user.nickname
       user_dic[:account]  = user.account
-      user_dic[:is_logged] = user.is_loggedin
+      user_dic[:is_logged] = user.isLoggedin
       user_dic[:sex] = user.sex
       user_dic[:image_url] = user.avatar.url.to_s
 
